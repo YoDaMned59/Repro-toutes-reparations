5524a323a8d290098c96d3f690ed2705982b0ade
YoDaMned59 <sduvivier59112@gmail.com>
2025-04-03 12:18:30 +0200

    Passage à HashRouter et simplification de la gestion des routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -15,6 +15,7 @@ function App() {
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/accueil" element={<Home />} />
             <Route path="/services" element={<Services />} />
             <Route path="/realisations" element={<Realisations />} />
             <Route path="/contact" element={<Contact />} />
