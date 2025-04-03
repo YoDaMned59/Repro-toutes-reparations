639b90998a13a3d970424968c62c6a23227ca02b
Sebastien Duvivier <sebastien.duvivier@outlook.fr>
2025-04-03 15:54:31 +0200

    Feat: ajout du menu burger responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Header.scss';
 import logo from '../assets/images/Logo.png';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="header">
       <div className="header__container">
@@ -10,19 +17,29 @@ const Header = () => {
           <img src={logo} alt="ArtisanPro Bâtiment Logo" className="header__logo-image" />
         </Link>
         
-        <nav className="header__nav">
+        <button 
+          className={`header__burger ${isMenuOpen ? 'open' : ''}`} 
+          onClick={toggleMenu}
+          aria-label="Menu principal"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        <nav className={`header__nav ${isMenuOpen ? 'open' : ''}`}>
           <ul className="header__nav-list">
             <li className="header__nav-item">
-              <Link to="/" className="header__nav-link">Accueil</Link>
+              <Link to="/" className="header__nav-link" onClick={toggleMenu}>Accueil</Link>
             </li>
             <li className="header__nav-item">
-              <Link to="/services" className="header__nav-link">Services</Link>
+              <Link to="/services" className="header__nav-link" onClick={toggleMenu}>Services</Link>
             </li>
             <li className="header__nav-item">
-              <Link to="/realisations" className="header__nav-link">Réalisations</Link>
+              <Link to="/realisations" className="header__nav-link" onClick={toggleMenu}>Réalisations</Link>
             </li>
             <li className="header__nav-item">
-              <Link to="/contact" className="header__nav-link">Contact</Link>
+              <Link to="/contact" className="header__nav-link" onClick={toggleMenu}>Contact</Link>
             </li>
           </ul>
         </nav>
